849c5d4373a7a4ed3b57ae4ae9b3c19bf38b7676
matthew <matt.stecko@gmail.com>
2021-01-24 13:23:21 -0500

    used inheritance

--- Box.js
+++ Box.js
@@ -1,27 +1,38 @@
-class Box {
-  constructor(x, y, width, height) {
-    var options = {
-        'restitution':0.8,
-        'friction':1.0,
-        'density':1.0
-    }
-    this.body = Bodies.rectangle(x, y, width, height, options);
-    this.width = width;
-    this.height = height;
+// class Box {
+//   constructor(x, y, width, height) {
+//     var options = {
+//         'restitution':0.8,
+//         'friction':1.0,
+//         'density':1.0
+//     }
+//     this.body = Bodies.rectangle(x, y, width, height, options);
+//     this.width = width;
+//     this.height = height;
     
-    World.add(world, this.body);
-  }
-  display(){
-    var pos =this.body.position;
-    var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
-    rectMode(CENTER);
-    strokeWeight(4);
-    stroke("green");
-    fill(255);
-    rect(0, 0, this.width, this.height);
-    pop();
-  }
-};
+//     World.add(world, this.body);
+//   }
+//   display(){
+//     var pos =this.body.position;
+//     var angle = this.body.angle;
+//     push();
+//     translate(pos.x, pos.y);
+//     rotate(angle);
+//     rectMode(CENTER);
+//     strokeWeight(4);
+//     stroke("green");
+//     fill(255);
+//     rect(0, 0, this.width, this.height);
+//     pop();
+//   }
+// };
+
+class Box extends BaseClass{
+constructor(x,y){
+super(x,y,70,70)
+
+this.image = loadImage("sprites/wood1.png");
+
+}
+
+
+}
